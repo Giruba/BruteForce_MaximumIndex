372554056b27aa26c79f31036c715ccee58b5002
Giruba Beulah SE <gibeulah@microsoft.com>
2019-05-01 23:59:27 +0530

    Add project files.

---
+++ MaximumIndex[J-I]/ArrayUtility.cs
@@ -0,0 +1,51 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MaximumIndex_J_I_
+{
+    public static class ArrayUtility
+    {
+        public static int[] GetArrayFromUser() {
+            int[] array = null;
+            Console.WriteLine("Enter the number of elements in the array");
+            try
+            {
+                int numberOfElements = int.Parse(Console.ReadLine().Trim());
+                Console.WriteLine("Enter the elements of the array separated by " +
+                    "space, comma or semi-colon");
+                String[] elements = Console.ReadLine().Split(' ', ',', ';');
+                array = new int[numberOfElements];
+                for (int index = 0; index < numberOfElements; index++) {
+                    array[index] = int.Parse(elements[index]);
+                }
+            }
+            catch (Exception exception) {
+                Console.WriteLine("Thrown exception is "+exception.Message);
+            }
+            return array;
+
+        }
+
+        public static int GetMaximumIndexInArray(int[] array) {
+            int arrayLength = array.Length;
+            int index = int.MinValue;
+            int minIndex = int.MaxValue;
+            int maxIndex = int.MinValue;
+
+            for (int arrayIndex = 0; arrayIndex < arrayLength; arrayIndex++) {
+                for(int comparingIndex = arrayIndex+1; comparingIndex < arrayLength; comparingIndex++)
+                if (array[comparingIndex] > array[arrayIndex] &&
+                        (comparingIndex - arrayIndex) > index) {
+                    index = comparingIndex - arrayIndex;
+                    minIndex = arrayIndex;
+                    maxIndex = comparingIndex;
+                }
+            }
+            Console.WriteLine("Min Element index is " + minIndex +
+                " and Max Element index is " + maxIndex);
+            return (maxIndex-minIndex);
+        }
+    }
+}
---
+++ MaximumIndex[J-I]/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace MaximumIndex_J_I_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Maximum index (j-i) in the array where element at j is greater than that at i!");
+            Console.WriteLine("--------------------------------------------------------------------");
+
+            int[] array = ArrayUtility.GetArrayFromUser();
+            Console.WriteLine("Maximum index [j-i] in the array is "+
+                ArrayUtility.GetMaximumIndexInArray(array));
+
+            Console.ReadLine();
+        }
+    }
+}
